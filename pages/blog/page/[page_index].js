8e4e46062449061f14bdda1d046e5199016afcbf
julie-desvaux <desvaux.julie@gmail.com>
2021-08-20 20:26:31 +0200

    Clean Up Get Posts

--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -1,12 +1,11 @@
 import fs from "fs";
 import path from "path";
-import matter from "gray-matter";
 
 import Layout from "@/components/Layout";
 import Post from "@/components/Post";
 import Pagination from "@/components/Pagination";
-import { sortByDate } from "@/utils/index.js";
 import { POSTS_PER_PAGE } from "@/config/index.js";
+import { getPosts } from "../../../lib/posts";
 
 export default function BlogPage({ posts, numPages, currentPage }) {
 	return (
@@ -48,20 +47,11 @@ export async function getStaticProps({ params }) {
 
 	const files = fs.readdirSync(path.join("posts"));
 
-	const posts = files.map((filename) => {
-		const slug = filename.replace(".md", "");
-		const markdownWithMeta = fs.readFileSync(path.join("posts", filename), "utf-8");
-		const { data: frontmatter } = matter(markdownWithMeta);
-
-		return {
-			slug,
-			frontmatter,
-		};
-	});
+	const posts = getPosts();
 
 	const numPages = Math.ceil(files.length / POSTS_PER_PAGE);
 	const pageIndex = page - 1;
-	const orderedPosts = posts.sort(sortByDate).slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE);
+	const orderedPosts = posts.slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE);
 
 	return {
 		props: {
