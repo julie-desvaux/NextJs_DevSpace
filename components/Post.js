81e15d43d18f8b1248a007ef916e6118b859c219
julie-desvaux <desvaux.julie@gmail.com>
2021-08-20 18:08:53 +0200

    Category Label Component

--- components/Post.js
+++ components/Post.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 
+import CategoryLabel from "./CategoryLabel";
+
 export default function Post({ post }) {
 	return (
 		<div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
@@ -13,7 +15,7 @@ export default function Post({ post }) {
 			/>
 			<div className="flex justify-between items-center">
 				<span className="font-light text-gray-600">{post.frontmatter.date}</span>
-				<div>{post.frontmatter.category}</div>
+				<CategoryLabel>{post.frontmatter.category}</CategoryLabel>
 			</div>
 			<div className="mt-2">
 				<Link href={`/blog/${post.slug}`}>
