350b9ce058087f976a1e6370386147987390fca1
julie-desvaux <desvaux.julie@gmail.com>
2021-08-21 12:20:56 +0200

    fix bug in production

--- pages/api/search.js
+++ pages/api/search.js
@@ -10,10 +10,12 @@ export default (req, res) => {
 		posts = require("../../cache/data").posts;
 	} else {
 		const files = fs.readdirSync(path.join("posts"));
+
 		posts = files.map((filename) => {
 			const slug = filename.replace(".md", "");
 
 			const markdownWithMeta = fs.readFileSync(path.join("posts", filename), "utf-8");
+
 			const { data: frontmatter } = matter(markdownWithMeta);
 
 			return {
--- scripts/cache.js
+++ scripts/cache.js
@@ -4,10 +4,12 @@ const matter = require("gray-matter");
 
 function postData() {
 	const files = fs.readdirSync(path.join("posts"));
+
 	const posts = files.map((filename) => {
 		const slug = filename.replace(".md", "");
 
 		const markdownWithMeta = fs.readFileSync(path.join("posts", filename), "utf-8");
+
 		const { data: frontmatter } = matter(markdownWithMeta);
 
 		return {
